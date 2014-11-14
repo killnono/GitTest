12d7b4df75c83fe7a41c9dfac320e3433e68b1e8
killnono <ckcanfly@gmail.com>
2014-11-14 11:08:18 +0800

    添加test.txt

---
+++ NTest/src/main/java/tv/guanghe/test/Test.java
@@ -0,0 +1,12 @@
+package tv.guanghe.test;
+
+/**
+ * Copyright (c) 2014 Guanghe.tv All right reserved.
+ * --------------------<-.->-----------------------
+ * Author:      Nono(陈凯)
+ * CreateDate:  14/11/13
+ * Description: exp...
+ * Version:     1.0.0
+ */
+public class Test {
+}
